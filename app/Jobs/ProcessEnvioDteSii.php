ceb23c31596c391039ba1394469de481a165506e
GatoMota <aaguirreu@utem.cl>
2024-05-09 11:35:31 -0400

    merge final

--- app/Jobs/ProcessEnvioDteSii.php
+++ app/Jobs/ProcessEnvioDteSii.php
@@ -11,7 +11,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
 
 class ProcessEnvioDteSii implements ShouldQueue
 {
